52a157996836f6ef34a77ffd1756d31e00afc10f
Kazuko Baynton <kazukobaynton@gmail.com>
2022-06-16 19:43:50 +0900

    add click event functions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,54 +8,60 @@ const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    value: 6,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    value: "1pm",
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    value: "Wednesday",
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
-  }
+    value: "2.3",
+  },
 ];
-
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    })); 
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panelArray = data.map( panel => {
-      return(
-        <Panel
-          key={panel.id}
-          id={panel.id}
-          label={panel.label}
-          value={panel.value}
-        />
-      )
-    })
+    // if focused only show that array and if not show all the panels
+    const panelArray = (this.state.focused ? data.filter((panel) => this.state.focused === panel.id) : data)
+      .map((panel) => {
+        return (
+          <Panel
+            key={panel.id}
+            label={panel.label}
+            value={panel.value}
+            onSelect={event => this.selectPanel(panel.id)}
+          />
+        );
+    });
 
-    return (
-        <main className={dashboardClasses}>
-          {panelArray}
-        </main>        
-    );
+    return <main className={dashboardClasses}>{panelArray}</main>;
   }
 }
 
